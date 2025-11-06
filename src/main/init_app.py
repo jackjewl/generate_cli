0d779ab5bab0f19e2880c1d1a4a2b0ed1143b5f3
name <mail@outllok.com>
2025-11-07 03:05:48 +0800

    init

--- src/main/init_app.py
+++ src/main/init_app.py
@@ -1,4 +1,6 @@
 
 from common import log
 def init_app(cfg):
-    log.info("init_app.init_app")
+    log.info("init_app.init_app finished")
+    log.info("this is config setting")
+    log.init(cfg)
--- src/main/run.py
+++ src/main/run.py
@@ -1,5 +1,12 @@
 
 from common import log
 
+import src.cron as cron
+
 def run(cfg):
     log.info("run.run")
+
+    cron.init(cfg)
+    cron.run(cfg)
+
+
